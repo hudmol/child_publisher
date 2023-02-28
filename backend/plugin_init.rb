363bba913302aa3c58af0ac3278d634917826c91
James Bullen <jambun@gmail.com>
2023-03-01 09:54:17 +1100

    Remove the type clause from the exclude call to exclude all notes with the preservica url

--- backend/plugin_init.rb
+++ backend/plugin_init.rb
@@ -1,5 +1,5 @@
 
-ChildPublishing.add_filter(Note.exclude{Sequel.like(:notes, '%"type":"otherfindaid"%') & Sequel.like(:notes, '%"content":"https://preservica.library.yale.edu%')})
+ChildPublishing.add_filter(Note.exclude{Sequel.like(:notes, '%"content":"https://preservica.library.yale.edu%')})
 
 ArchivalObject.include(ChildPublisher)
 
